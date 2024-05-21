be5cf1d7ba30ca5f125b79218f9ee69d0c669473
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-05-21 15:15:54 -0300

    resolucao

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', event => {
+  const resolucao = `${window.innerWidth} x ${window.innerHeight}`
+  document.getElementById('resolucao').textContent = resolucao
+})
